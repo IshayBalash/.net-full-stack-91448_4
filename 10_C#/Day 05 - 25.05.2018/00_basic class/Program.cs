0e73b6e84a680390849a84e511e4d0b78ae4bb6c
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-05-25 10:29:55 +0300

    914484

---
+++ 10_C#/Day 05 - 25.05.2018/00_basic class/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _00_basic_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flight flight1 = new Flight();
+            Flight flight2 = new Flight(-400);
+            flight1.FlightPrice = -500;
+            bool isCorrectPrice=flight1.CheckFlightPrice(500);
+
+            Console.WriteLine(isCorrectPrice);  // --> true
+        }
+    }
+}
