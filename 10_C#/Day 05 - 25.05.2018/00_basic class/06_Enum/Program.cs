916cbefd8413618dcbc0ac469755811fe06078e9
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-05-25 13:15:56 +0300

    914484

---
+++ 10_C#/Day 05 - 25.05.2018/00_basic class/06_Enum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeekendHobies myWeekendHobies = WeekendHobies.Learning;
+            Console.WriteLine($"this weekend I am planning {myWeekendHobies} and {WeekendHobies.Coding}");
+        }
+    }
+}
+
+
+/*
+OUTPUT:
+_____________________________________
+
+this weekend I am planning Learning and Coding    
+*/
