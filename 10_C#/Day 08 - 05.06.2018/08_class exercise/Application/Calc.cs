6d0ca0505706140b46fa8f728a4e30757c4d6324
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-06-05 21:54:55 +0300

    914484

---
+++ 10_C#/Day 08 - 05.06.2018/08_class exercise/Application/Calc.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApplication1
+{
+
+    delegate bool CompareHandler(int n1, int n2);
+
+    class Calc
+    {
+        public event CompareHandler CompareEventHandler;
+
+        private int successCounter;
+        public int SuccessCounter
+        {
+            get { return successCounter; }
+        }
+
+
+        public bool TestNumbers(int n1, int n2)
+        {
+            bool result=false;
+
+            if (CompareEventHandler != null)
+            {
+                result = CompareEventHandler(n1, n2);
+                successCounter += result ? 1 : 0;
+            }
+           
+            return result;
+        }
+
+    }
+
+
+}
---
+++ 10_C#/Day 08 - 05.06.2018/08_class exercise/Application/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc[] calcArray = {new Calc(),new Calc()};
+
+            calcArray[0].CompareEventHandler += (num1, num2) => num1 > num2;
+
+            calcArray[1].CompareEventHandler += (num1, num2) => num1 == num2;
+
+
+            List<int[]> numberPairsArray = new List<int[]>();
+
+            numberPairsArray.AddRange(new int[][]{
+                new int[] { 1, 2 },
+                new int[] { 1, 1 },
+                new int[] { 5,8 },
+                new int[] { 3,3 },
+                new int[] { 2,7 },
+                new int[] { 6,6 }
+            } );
+
+            foreach (int[] item in numberPairsArray)
+            {
+                foreach (Calc calc in calcArray)
+                {
+                    bool res=calc.TestNumbers(item[0], item[1]);
+                    Console.WriteLine($"result of calc.TestNumbers({item[0]}, {item[1]}) is {res}");
+                }
+                Console.WriteLine("--------------------------------------");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"calcArray[0] success counter {calcArray[0].SuccessCounter}");
+            Console.WriteLine($"calcArray[1] success counter {calcArray[1].SuccessCounter}");
+
+        }
+    }
+}
+
+
+/*
+
+OUTPUT
+___________________________________
+
+result of calc.TestNumbers(1, 2) is False
+result of calc.TestNumbers(1, 2) is False
+--------------------------------------
+result of calc.TestNumbers(1, 1) is False
+result of calc.TestNumbers(1, 1) is True
+--------------------------------------
+result of calc.TestNumbers(5, 8) is False
+result of calc.TestNumbers(5, 8) is False
+--------------------------------------
+result of calc.TestNumbers(3, 3) is False
+result of calc.TestNumbers(3, 3) is True
+--------------------------------------
+result of calc.TestNumbers(2, 7) is False
+result of calc.TestNumbers(2, 7) is False
+--------------------------------------
+result of calc.TestNumbers(6, 6) is False
+result of calc.TestNumbers(6, 6) is True
+--------------------------------------
+
+calcArray[0] success counter 0
+calcArray[1] success counter 3
+     
+*/
