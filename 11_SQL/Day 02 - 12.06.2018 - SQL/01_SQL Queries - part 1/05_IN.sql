bf2ec1761fb6c7f1519f09a4ac62c9f5c3118763
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-06-12 21:41:08 +0300

    914484

---
+++ 11_SQL/Day 02 - 12.06.2018 - SQL/01_SQL Queries - part 1/05_IN.sql
@@ -0,0 +1,30 @@
+/*
+--************************* IN *************************
+
+The WHERE clause is used to filter records with a specified condition.
+ 
+
+Common WHERE Operator:
+-----------------------------------
+IN			|	Specify multiple possible values
+
+
+*/
+
+USE Northwind
+GO
+
+--select all products that are named "Chai", "Chang" or "Aniseed Syrup"
+SELECT * 
+FROM Products
+WHERE ProductName IN ('Chai', 'Chang', 'Aniseed Syrup')
+
+
+
+--select all products that are NOT named "Chai", "Chang" or "Aniseed Syrup"
+SELECT * 
+FROM Products
+WHERE ProductName NOT IN ('Chai', 'Chang', 'Aniseed Syrup') 
+
+
+
