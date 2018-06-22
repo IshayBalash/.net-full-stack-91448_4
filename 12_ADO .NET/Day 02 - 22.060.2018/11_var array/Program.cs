90589a83712b169f8d0c9077eef6b621c1bbdf3a
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-06-22 13:18:33 +0300

    914484

---
+++ 12_ADO .NET/Day 02 - 22.060.2018/11_var array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11_var_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //מערך של אובייקטים אנונימיים
+            //for "implicitly - typed array" we must to craete all the new{ } object with the same structer
+              var list = new[]{
+                new { Name="Alice", Age=13},
+                new { Name="Alice", Age=14}
+            };
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Name+ " "+item.Age);
+            };
+
+
+            object[] list1 = new[]{
+                new { Name="Alice", Age=13},
+                new { Name="Alice", Age=14}
+            };
+
+            foreach (var item in list1)
+            {
+                //Console.WriteLine(item.Name + " " + item.Age);  // --> COMPILATION ERROR
+            }
+        }
+    }
+}
