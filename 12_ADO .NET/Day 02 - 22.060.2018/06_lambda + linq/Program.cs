6e949c73dd594542af2fdf1f1b183234974aba0e
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-06-22 12:18:57 +0300

    914484

---
+++ 12_ADO .NET/Day 02 - 22.060.2018/06_lambda + linq/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//LINQ  =  Language-Integrated Query  (CAN RUN ON C# IEnumerable OBJECTS)
+namespace _04_Basic_LINQ
+{
+    class Product
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                List <Product> srcList = new List<Product>
+            {
+                new Product {ProductName="Pizza",UnitPrice=20 },
+                new Product {ProductName="Chips",UnitPrice=12 },
+                new Product {ProductName="Water",UnitPrice= 6},
+                new Product {ProductName="Burger",UnitPrice=50 },
+                new Product {ProductName="HotDog",UnitPrice=14 }
+            };
+
+            //בחירת כל המוצרים שמחירם גדול מ15
+            IEnumerable<Product> distList1 = srcList.Where((Product prod) => { return prod.UnitPrice > 15; });
+
+
+            foreach (Product item in distList1)
+            {
+                Console.WriteLine($"ProductName:{item.ProductName}  , UnitPrice:{item.UnitPrice}");
+            }
+
+
+                //בחירת שמות כל המוצרים שמחירם גדול מ15
+                IEnumerable<string> distList2 = srcList
+                    .Where((Product prod) => { return prod.UnitPrice > 15;})
+                    .Select((Product prod) => { return prod.ProductName; });
+
+
+            foreach (string item in distList2)
+            {
+                Console.WriteLine($"ProductName:{item}");
+            }
+
+        }
+    }
+}
+
+/*
+OUTPUT:
+______________________________
+
+ProductName:Pizza  , UnitPrice:20
+ProductName:Burger  , UnitPrice:50
+
+ProductName:Pizza
+ProductName:Burger
+     
+*/
---
+++ 12_ADO .NET/Day 02 - 22.060.2018/07_select and where/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_select_and_where
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+            //פילטור המערך למספרים זוגיים בלבד
+            IEnumerable<int> res1 = list.Where((int i) => { return i % 2 == 0; });
+
+            Console.WriteLine("res1--------------------");
+            foreach (int item in res1)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //הצגת הריבוע של כל מספר במערך
+            IEnumerable<int> res2 = list.Select((int i) => { return i * i; });
+
+            Console.WriteLine("res2--------------------");
+            foreach (int item in res2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            //הצגת הריבוע של כל מספר זוגי במערך
+            IEnumerable<int> res3 = list
+                .Where((int i) => { return i % 2 == 0; })
+                .Select((int i) => { return i * i; });
+
+            Console.WriteLine("res3--------------------");
+            foreach (int item in res3)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //הצגת הריבוע של כל מספר במערך
+            IEnumerable<string> res4 = list.Select((int i) => { return $"number is {i}"; });
+
+            Console.WriteLine("res4--------------------");
+            foreach (string item in res4)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
+
+/*
+ OUTPUT:
+ ________________
+res1--------------------
+2
+4
+6
+
+res2--------------------
+1
+4
+9
+16
+25
+36
+
+res3--------------------
+4
+16
+36
+
+res4--------------------
+number is 1
+number is 2
+number is 3
+number is 4
+number is 5
+number is 6
+*/
