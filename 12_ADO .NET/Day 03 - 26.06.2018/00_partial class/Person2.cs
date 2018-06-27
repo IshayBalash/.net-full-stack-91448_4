510e978c618f25f0f0d1c9a6319a090aefb19db5
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-06-27 09:26:20 +0300

    914484

---
+++ 12_ADO .NET/Day 03 - 26.06.2018/00_partial class/Person2.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_partial_class
+{
+    partial class Person
+    {
+        public override string ToString()
+        {
+            return $"{Name} is {Age} years old, IsMale:{IsMale}";
+        }
+    }
+}
